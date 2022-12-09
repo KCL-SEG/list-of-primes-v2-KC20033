0da4364df5f4600bf8620380b5d9c9b4766591f1
Kyron Caesar <dragonitezball@gmail.com>
2022-12-09 11:52:06 +0000

    Add solution

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+    start = 2
+    if number_of_primes > 0:
+
+        while len(list) != number_of_primes:
+        
+            if checkPrime(start):
+                list.append(start)
+            start += 1
+        
+    else:
+        raise ValueError        
+        
     return list
+
+
+def checkPrime(x):
+    if x <= 1 :
+        return False
+    for i in range (2, x):
+        if x % i == 0:
+            return False
+    return True
